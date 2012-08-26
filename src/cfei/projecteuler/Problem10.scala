09bf2082288bec130b6a78d28ae500314788c598
Chris Fei <cfei18@gmail.com>
2012-08-26 12:18:35 -0400

    Problem 10

--- src/cfei/projecteuler/Problem10.scala
+++ src/cfei/projecteuler/Problem10.scala
@@ -2,6 +2,12 @@ package cfei.projecteuler
 import cfei.projecteuler.util.Utils
 
 object Problem10 {
+  
+  /**
+   * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+   * 
+   * Find the sum of all the primes below two million.
+   */
 
   def main(ags : Array[String]) {
     val sum = 2.until(2000000).filter(n => Utils.isPrime(n)).foldLeft(0L)((result, n) => {
