9a2f941ed86868a125d16f0d6903e6537cc2e94e
Chris Fei <cfei18@gmail.com>
2012-08-25 22:13:07 -0400

    Problem 4

--- src/cfei/projecteuler/Problem4.scala
+++ src/cfei/projecteuler/Problem4.scala
@@ -3,11 +3,19 @@ package cfei.projecteuler
 object Problem4 {
 
   def main(args : Array[String]) {
-    
+    var max = 0
+    for(i <- 100.until(999); j <- i.until(999)) {
+      val num = i * j
+      if(num > max && isPalindrome(num)) {
+       max = num 
+      }
+    }
+    println(max)
   }
   
   def isPalindrome(num : Int) : Boolean = {
-    
-    return true;
+    val reverse = num.toString().reverse
+    return reverse equals num.toString()
   }
+  
 }
