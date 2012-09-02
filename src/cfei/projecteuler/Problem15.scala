61c768fcec8e3eca1c7342de352d8d871ed6a849
Chris Fei <cfei18@gmail.com>
2012-09-02 14:38:31 -0400

    Problem 15

---
+++ src/cfei/projecteuler/Problem15.scala
@@ -0,0 +1,22 @@
+package cfei.projecteuler
+
+object Problem15 {
+
+  /**
+   * Starting in the top left corner of a 2x2 grid, there are 6 routes 
+   * (without backtracking) to the bottom right corner.
+   * 
+   * How many routes are there through a 2020 grid?
+   */
+  
+  val side:BigInt = BigInt(20)
+  def main(args:Array[String]) {
+    //Thank you combinatorics class - the answer is just 40 choose 20
+    val moves = 2 * 20
+    println(factorial(moves) / (factorial(moves - side) * factorial(side)))
+  }
+  
+  def factorial(num:BigInt) : BigInt = {
+    BigInt(2).until(num+1).reduce(_*_)
+  }
+}
