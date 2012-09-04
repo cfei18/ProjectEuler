7df061c07d93f7e06ec9b6dd027a995e5a5c0c1b
Chris Fei <cfei18@gmail.com>
2012-09-04 15:56:08 -0400

    Comments for problems 16 and 20

--- src/cfei/projecteuler/Problem16.scala
+++ src/cfei/projecteuler/Problem16.scala
@@ -2,6 +2,12 @@ package cfei.projecteuler
 
 object Problem16 {
 
+  /**
+   * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+   * 
+   * What is the sum of the digits of the number 2^1000?
+   */
+  
   def main(args:Array[String]) {
     var num = BigInt(2).pow(1000)
     println (num.toString filter {_.isDigit} map {_.asDigit} reduce {_+_})
--- src/cfei/projecteuler/Problem20.scala
+++ src/cfei/projecteuler/Problem20.scala
@@ -4,6 +4,15 @@ import cfei.projecteuler.util.Utils
 
 object Problem20 {
 
+  /**
+   * n! means n * (n- 1) * ... * 3 * 2 * 1
+   * For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
+   * and the sum of the digits in the number 10! 
+   * is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+   * 
+   * Find the sum of the digits in the number 100!
+   */
+  
   def main(args:Array[String]) {
     var num = Utils.factorial(100)
     println (num.toString filter {_.isDigit} map {_.asDigit} reduce {_+_})
